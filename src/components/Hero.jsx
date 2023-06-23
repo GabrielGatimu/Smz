e0e5caad100254bc6b31a14a8eb7af1a684b71ee
GabrielGatimu <geraldg652@gmail.com>
2023-06-23 16:50:00 +0300

    added hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="smz_logo" className="w-28 object-contain" />
+        <button type="button" onClick={() => window.open('https://github.com/gabrielgatimu/Smz')} className="black_btn">
+            github
+        </button>
+        </nav>
+
+        <h1 className="head_text">
+            Summarize Articles with <br /> <span className="orange_gradient">OpenAI GPT-4</span>
+        </h1>
+        </header>
+  )
+}
+
+export default Hero;
